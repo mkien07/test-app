9cde1f2b9dacb1863962bcdbbd6e38f648bbeba6
mkien07 <mkien0119@gmail.com>
2025-10-23 00:17:35 +0700

    Cập nhật code mới

--- models/User.js
+++ models/User.js
@@ -4,12 +4,13 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   email: String,
+  phone: String,
   money: { type: Number, default: 0 },
   isLocked: { type: Boolean, default: false },
-  registerIP: String,
-  lastLoginIP: String,
+  registerIP: { type: String, default: "unknown" },
+  lastLoginIP: { type: String, default: "unknown" },
   registerTime: { type: Date, default: Date.now },
-  activeSession: String, // lưu token hiện tại
+  activeSession: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
