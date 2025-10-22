a3bd8dec33c073e9e2e4841434ae692db160a136
mkien07 <mkien0119@gmail.com>
2025-10-22 23:19:49 +0700

    Cập nhật code mới

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 // ✅ API Routes
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/user", require("./routes/user"));
 
